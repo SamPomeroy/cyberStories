6065b89fc8cc72a69b0030fb0c82722c0bf4e511
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2024-02-21 21:07:41 -0600

    @media???

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-// const body = document.querySelector('body');
-
-// const parent = document.createElement('div');
-// parent.className = 'parent';
-
-// const child = document.createElement('div');
-// child.className = 'child';
-
-
 const topStories = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
-
-//set const for parent to append to
 const headlines = document.querySelector('#headlines')
-
 function fetchData(response){
     let apiClips = `https://hacker-news.firebaseio.com/v0/item/${response}.json?print=pretty`
     return fetch(apiClips)
@@ -26,19 +14,16 @@ fetch(topStories)
         for(let item of array){
             newArray.push(fetchData(item))
         }
-        console.log(newArray)
-
-        //promise.all shorthand from Kyle
+        // console.log(newArray)
         Promise.all(newArray)
         .then((results) => Promise.all(results.map(res => res.json())))
         .then((data)=>{
-            console.log(data)
-            
-            //loop, create element (clips), append to parent, set innerHTML for clips 
+            // console.log(data)
             for (let item of data){
                 let clips = document.createElement('li')
                 clips.innerHTML = `<a href=${item.url}>${item.title}</a></br>By: ${item.by}, Score: ${item.score}, Comments: ${item.descendants}`
                 headlines.appendChild(clips)
+                console.log('hello')
             }
         })
     })
